5966e7a6f4232b81a2b72adb91973101f35d35e4
keisks <keisks5@gmail.com>
2013-06-24 20:44:40 +0900

    forward step

--- ws/src/main/scala/WordSegmentation.scala
+++ ws/src/main/scala/WordSegmentation.scala
@@ -13,7 +13,7 @@ object WordSegmentation {
     val inputFile = Source.fromFile(inputPath, "UTF-8")
 
     // create map w
-    var model = mutable.Map[String, Float]() //weightMap
+    var model = mutable.Map[String, Double]() //weightMap
 
     // load model
     for (line <- modelFile.getLines()){
@@ -24,30 +24,40 @@ object WordSegmentation {
       model.update(word, prob)
     }
 
-    // 
     // viterbi algorithm
     for (line <- inputFile.getLines()){
 
       // Forward step
       val sentence = line.stripLineEnd
 
-      val best_score = mutable.ListBuffer[Double]()
-      val best_edge = mutable.ListBuffer[Int]()
+      val best_score = mutable.ListBuffer[Double](0.0)
+      val best_edge = mutable.ListBuffer((-1, 0))
 
-      for (word_end <- 1 until sentence.length){
+      for (word_end <- 1 to sentence.length){
         best_score += 99999.0d
+        best_edge += ((word_end-1, word_end))
 
         for (word_begin <- 0 to word_end-1) {
-          string = line.substring(word_begin, word_end)
+          val string = line.substring(word_begin, word_end)
 
-          if model.contains(string) 
+          if (model.contains(string) || string.length == 1){
+            val probability = model(string)
+            val tmp_score = best_score(word_begin) - (math.log(probability)/math.log(2))
 
-
-
+            if (tmp_score < best_score(word_end)) {
+              best_score(word_end) = tmp_score
+              best_edge(word_end) = ((word_begin, word_end))
+            }
+          }
+          //println(word_begin)
+          //println(word_end)
+          //println(best_score)
+          //println(best_edge)
         }
-
-
       }
+      
+      // Backward step
+
 
 
      
